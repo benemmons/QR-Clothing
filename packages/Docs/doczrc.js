a966b5ea0285a5cdffc4b9ba5c5eabdcc747592c
Ben Emmons <ben@emmons.biz>
2019-05-29 04:33:57 +0100

    Added Bulma CSS for styling

--- packages/Docs/doczrc.js
+++ packages/Docs/doczrc.js
@@ -1,4 +1,15 @@
 export default {
     title: 'QR-Clothing',
     description: "Redefining the capabilities of 'fashion' through abstract, internet-based techwear. The NQX Line gives anyone who scans your clothing the ability to send you an anonymous message. Prepare for unparalleled levels of transparency in a real-world setting.",
+    htmlContext: {
+        head: {
+          links: [
+            {
+              rel: 'stylesheet',
+              href:
+                'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css',
+            },
+          ],
+        },
+      },
 }
