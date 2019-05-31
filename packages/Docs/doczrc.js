2a9fce07f1f46cf95990182b5c65d3dbaabaf8a1
Ben Emmons <ben@emmons.biz>
2019-05-31 23:24:34 +0100

    [DOCS] Changed build directory

--- packages/Docs/doczrc.js
+++ packages/Docs/doczrc.js
@@ -1,6 +1,7 @@
 export default {
     title: 'QR-Clothing',
     description: "Redefining the capabilities of 'fashion' through abstract, internet-based techwear. The NQX Line gives anyone who scans your clothing the ability to send you an anonymous message. Prepare for unparalleled levels of transparency in a real-world setting.",
+    dest: '/build',
     htmlContext: {
         head: {
           links: [
