2e14299c111338a6ffc6ac97cd0de83d9d49e0f7
Ben Emmons <ben@emmons.biz>
2019-07-22 01:34:47 +0100

    [WebApp] ID-based routing

---
+++ packages/WebApp/src/components/Home.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {
+  Layout,
+  Page,
+  FooterHelp,
+  Card,
+  Link,
+  Button,
+  FormLayout,
+  TextField,
+  AccountConnection,
+  ChoiceList,
+  SettingToggle,
+} from '@shopify/polaris';
+
+export default class Home extends Component {
+    render(){
+        return(      
+          <Page
+          title="Home"
+        >
+        </Page>
+)
+    }
+}
+
---
+++ packages/WebApp/src/components/Profile.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {
+  Layout,
+  Page,
+  FooterHelp,
+  Card,
+  Link,
+  Button,
+  FormLayout,
+  TextField,
+  AccountConnection,
+  ChoiceList,
+  SettingToggle,
+} from '@shopify/polaris';
+
+export default class Profile extends Component {
+    render(){
+        return(      
+          <Page
+          title="Profie #589902"
+          breadcrumbs={[{
+            content: 'Home',
+            url: '/'  // host webapp on subdomain then have this link to root domain to bridge react-shopify gap
+          }]}
+          primaryAction={{content: 'New Response'}}
+        >
+        </Page>
+)
+    }
+}
+
